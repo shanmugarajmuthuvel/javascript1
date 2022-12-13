df73464c1e0e31791f99c62b7b99bd1e8827e6d3
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2022-12-13 17:12:58 +0530

    if-else

---
+++ test.js
@@ -0,0 +1,11 @@
+   var sha=parseInt(prompt("enter ther value"))
+   if(sha>=0){
+       document.write("The Number is Positive")
+	   }
+   else{
+       document.write("The Number is Negative")
+}	   
+   var a=15
+   var b=20
+   var c=a+b
+   console.log(c)
